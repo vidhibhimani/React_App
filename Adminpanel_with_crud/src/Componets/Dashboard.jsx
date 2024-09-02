d29f3112904370626fe6990e8617ded69117773b
vidhibhimani <bhimanisahil045@gmail.com>
2024-09-02 18:48:11 +0530

    admin finle project

--- Adminpanel_with_crud/src/Componets/Dashboard.jsx
+++ Adminpanel_with_crud/src/Componets/Dashboard.jsx
@@ -5,66 +5,7 @@ export const Dashboard = () => {
     <div className="main">
       <div className="container-fluid">
         <div className="row">
-          {/* Projects Section */}
-          {/* <div className="col-lg-6">
-            <div className="card shadow mb-4">
-              <div className="card-header py-3">
-                <h6 className="m-0 font-weight-bold text-primary">Projects</h6>
-              </div>
-              <div className="card-body">
-                <h4 className="small font-weight-bold">
-                  Server Migration <span className="float-right">20%</span>
-                </h4>
-                <div className="progress mb-4">
-                  <div
-                    className="progress-bar bg-danger"
-                    role="progressbar"
-                    style={{ width: "20%" }}
-                  ></div>
-                </div>
-                <h4 className="small font-weight-bold">
-                  Sales Tracking <span className="float-right">40%</span>
-                </h4>
-                <div className="progress mb-4">
-                  <div
-                    className="progress-bar bg-warning"
-                    role="progressbar"
-                    style={{ width: "40%" }}
-                  ></div>
-                </div>
-                <h4 className="small font-weight-bold">
-                  Customer Database <span className="float-right">60%</span>
-                </h4>
-                <div className="progress mb-4">
-                  <div
-                    className="progress-bar bg-primary"
-                    role="progressbar"
-                    style={{ width: "60%" }}
-                  ></div>
-                </div>
-                <h4 className="small font-weight-bold">
-                  Payout Details <span className="float-right">80%</span>
-                </h4>
-                <div className="progress mb-4">
-                  <div
-                    className="progress-bar bg-info"
-                    role="progressbar"
-                    style={{ width: "80%" }}
-                  ></div>
-                </div>
-                <h4 className="small font-weight-bold">
-                  Account Setup <span className="float-right">Complete!</span>
-                </h4>
-                <div className="progress">
-                  <div
-                    className="progress-bar bg-success"
-                    role="progressbar"
-                    style={{ width: "100%" }}
-                  ></div>
-                </div>
-              </div>
-            </div>
-          </div> */}
+          
 
           {/* Illustrations Section */}
           <div className="col-lg-12 ms-4">
